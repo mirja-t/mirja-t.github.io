1d6e9e8606c4b45a1adaf765a18b13fb5cf5efb6
mirja-t <offensichtbar.mirja@gmail.com>
2026-04-30 10:52:41 +0200

    Add new SVG icons and update asset imports
    - Added new SVG icons: icon-tailwind, icon-testing, icon-ts, icon-ui, icon-uikit, icon-vite, icon-vscode, icon-wp, icon-xd, icon-xing, mt-logo, project-in.
    - Updated import paths for SVG icons in icons.js to use alias '@/' for better path resolution.
    - Modified tsconfig.json to include path mapping for '@/*' to './src/*'.
    - Updated vite.config.ts and vitest.config.ts to resolve alias '@' for consistent module resolution.

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,15 +1,15 @@
-import css from "/images/design/icon-css.svg";
-import git from "/images/design/icon-git.svg";
-import html from "/images/design/icon-html.svg";
-import js from "/images/design/icon-js.svg";
-import php from "/images/design/icon-php.svg";
-import react from "/images/design/icon-react.svg";
-import sass from "/images/design/icon-sass.svg";
-import sql from "/images/design/icon-sql.svg";
-import ui from "/images/design/icon-ui.svg";
-import testing from "/images/design/icon-testing.svg";
-import ts from "/images/design/icon-ts.svg";
-import email from "/images/design/icon-email.svg";
+import css from "@/assets/images/design/icon-css.svg";
+import git from "@/assets/images/design/icon-git.svg";
+import html from "@/assets/images/design/icon-html.svg";
+import js from "@/assets/images/design/icon-js.svg";
+import php from "@/assets/images/design/icon-php.svg";
+import react from "@/assets/images/design/icon-react.svg";
+import sass from "@/assets/images/design/icon-sass.svg";
+import sql from "@/assets/images/design/icon-sql.svg";
+import ui from "@/assets/images/design/icon-ui.svg";
+import testing from "@/assets/images/design/icon-testing.svg";
+import ts from "@/assets/images/design/icon-ts.svg";
+import email from "@/assets/images/design/icon-email.svg";
 
 const ICONS = {
     css,
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 // vite.config.js or vite.config.ts
-export default defineConfig({
-    base: "/mirja-tschakarov.de/",
+export default defineConfig(({ command }) => ({
+    base: command === "build" ? "/mirja-tschakarov.de/" : "/",
     build: {
         outDir: "docs",
     },
-});
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+}));
