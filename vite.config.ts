1c62ba9435a086bdbe1d183f0f78d1a75dfe5a79
mirja-t <offensichtbar.mirja@gmail.com>
2026-04-30 10:08:14 +0200

    feat: add GitHub Actions workflow for frontend testing and update test script in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // vite.config.js or vite.config.ts
 export default defineConfig({
+    base: "/mirja-tschakarov.de/",
     build: {
         outDir: "docs",
     },
